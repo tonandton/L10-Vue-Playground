d7b04ac5f19da500e2ed7a92006278cf2f762cfb
tonandton <www.sorasakok@gmail.com>
2024-12-01 21:47:33 +0700

    upload playground project

---
+++ backend/src/components/HelloWorld.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { ref, computed } from 'vue'
+import store from "../store"
+
+const test = computed(() => store.state.test)
+
+defineProps({
+    msg: String,
+})
+
+const count = ref(0)
+</script>
+
+<template>
+    <h1 class="bg-green-500">{{ msg }}</h1>
+
+    {{ test }}
+
+
+</template>
+
+<style scoped>
+.read-the-docs {
+    color: #888;
+}
+</style>
---
+++ backend/src/views/Dashboard.vue
@@ -0,0 +1,11 @@
+<template>
+    <h1>DashBoard</h1>
+</template>
+
+<script>
+export default {
+    name: "Dashboard"
+}
+</script>
+
+<style scoped></style>
